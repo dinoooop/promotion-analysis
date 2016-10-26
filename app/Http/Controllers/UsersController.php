64d9fad675ba597a75fb7325cbd87a7ef613a7e1
dinoooop <dinoooop@gmail.com>
2016-10-26 15:14:03 +0530

    model hash added

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 use App\User;
 use App\Stock;
