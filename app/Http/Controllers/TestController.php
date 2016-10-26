caa799a871eb70c916b1916f4f3c1fe4f9d47793
dinoooop <dinoooop@gmail.com>
2016-10-26 17:46:56 +0530

    query testing

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use App\Dot;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends BaseController {
 
@@ -14,8 +15,15 @@ class TestController extends BaseController {
     }
 
     function test() {
-        
-        echo csrf_token();
+        //$users = DB::table('users')->select('name', 'email as user_email')->get();
+        $records = DB::connection('redshift')
+                ->table('nwl_pcm.sap_material_additional')
+                ->first();
+//        $records = DB::connection('pgsql')
+//                ->table('users')
+//                ->first();
+        echo '<pre>', print_r($records), '</pre>';
+        exit();
         
     }
 
