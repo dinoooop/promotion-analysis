60f079e696d15388795462ce68da4be3fb43b61a
dinoooop <dinoooop@gmail.com>
2016-10-31 21:36:15 +0530

    sample psql 5

--- app/Sdcalc.php
+++ app/Sdcalc.php
@@ -26,8 +26,11 @@ class Sdcalc {
         $psql_date = $calendar->input($promo_start_date, $promo_end_date);
         //$sql = Block::prepare_psql($material_id, $psql_date);
         $sql = Block::sample_psql();
-        DB::setFetchMode(\PDO::FETCH_ASSOC);
-        $records = DB::connection('redshift')->select($sql);
+        
+        $query = DB::connection('redshift')->setFetchMode(\PDO::FETCH_ASSOC);
+        $records = $query->select($sql);
+        
+        
         
         echo '<pre>', print_r($records), '</pre>';
 
